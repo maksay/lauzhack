86ae38457ded7055d9568a9f5c2645495ef0827e
Mihail Tarigradschi <m.tarigradschi@gmail.com>
2017-11-12 10:52:41 +0100

    fix button class

--- button.py
+++ button.py
@@ -5,7 +5,7 @@ import time
 FINAL_SCALE_FACTOR = 3
 
 class Button:
-  def __init__(self, timeout, f, pos, radius):
+  def __init__(self, timeout, f, pos, radius, message = None):
     self.lastTime = 0
     self.timeout = timeout
     self.eps = 0
@@ -13,12 +13,16 @@ class Button:
     self.pos = pos
     self.radius = radius
     
+    self.text = message
+    
     self.function = f
     
   def draw(self, img):
     scaled_pos = (self.pos[0] * FINAL_SCALE_FACTOR, self.pos[1] * FINAL_SCALE_FACTOR)
     img = cv2.circle(img, scaled_pos, FINAL_SCALE_FACTOR * self.radius, [189,233,0], 4)
     img = cv2.circle(img, scaled_pos, int(FINAL_SCALE_FACTOR * self.radius*self.eps), [221+(221-189)*self.eps,126+(233-128)*self.eps,0], -1)
+    if self.text is not None:
+      cv2.putText(img, self.text, (scaled_pos[0]-FINAL_SCALE_FACTOR*self.radius//2, scaled_pos[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255,255,255))
     return img
 
   def checkPressed(self, img, pos1, pos2):
